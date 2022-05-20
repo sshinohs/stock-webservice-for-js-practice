0b8452390044d975630a4861209187192fe9a5df
sshinohs <sshinohs@gmail.com>
2022-05-20 14:02:48 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fs');
+
+const app = http.createServer(function(request, response) {
+    let url = request.url;
+    if(request.url == '/') {
+        url = '/index.html';
+    }
+    if(request.url == '/favicon.ico') {
+        return response.writeHead(404);
+    }
+    response.writeHead(200);
+    console.log('hing');
+    response.end(fs.readFileSync(__dirname + url));
+});
+app.listen(3000);
